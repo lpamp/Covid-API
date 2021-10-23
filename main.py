c0efcd09fa53785806d25ad3fc51c641e7c11b14
LucyPamp <92190643+lpamp@users.noreply.github.com>
2021-10-23 01:00:06 +0000

    Per capita function

--- main.py
+++ main.py
@@ -4,6 +4,24 @@ data = requests.get("https://covid-api.mmediagroup.fr/v1/cases")
 
 d = data.json()
 
+def percapita(infected, total):
+
+  #p = proportion
+
+  p = infected / total 
+  p = round(p, 5)
+  #Get percent first 
+  print("Population: ", total)
+  print("Percentage: ", round(p * 100, 4), '%')
+
+  #Per capita 
+  answer = p * 100000
+
+
+  print("Per Capita (100,000)", answer , "people has COVID.")
+
+
+
 #Ask for an input 
 print("Enter a place")
 country = input("What country are you looking for?")
@@ -29,6 +47,7 @@ for x in d:
 
     print("Confirmed cases", x, ":",d.get(x).get("All").get("confirmed"))
 
+    percapita(d.get(x).get("All").get("confirmed"),d.get(x).get("All").get("population"))
 
 
 
@@ -46,3 +65,28 @@ for x in d:
 #*****per capita******
 
 #Percentage by per capita 
+
+
+
+#before, it would be unequal to compare because
+#i.e india vs us: india has more in pop than us.
+#To equal the playing field, get rid of population and turn it into per capita 
+
+#per capita would be per 100,000 people in a country
+
+
+
+#example 34.1M India v 150K Australia
+
+#Per capita percentage * x amount of people compared 
+
+# Australia 150,000/ 25,700,000 = 0.584% = 
+#0.00584 * per 100,000 people = 584
+# India 34,100 / 1,380,000 = 2.471%
+# 0.0247 * per 100,000 people = 2471
+
+#proportion = decimal number of a ratio/fraction\
+
+
+
+
